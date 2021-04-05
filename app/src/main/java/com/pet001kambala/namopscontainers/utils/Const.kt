61ccca36db9f8c72656db323cdf515dff9b735b5
Petrus Kambala <pet001kambala@gmail.com>
2021-04-05 18:49:13 +0200

    cleaned up const interface

--- app/src/main/java/com/pet001kambala/namopscontainers/utils/Const.kt
+++ app/src/main/java/com/pet001kambala/namopscontainers/utils/Const.kt
@@ -2,24 +2,19 @@ package com.pet001kambala.namopscontainers.utils
 
 internal interface Const {
     companion object {
-        val DefaultJobCardNo: String = "JA000"
-        val JOB_CARD: String = "Job card"
-        val TRIP: String = "Trip"
-        val IS_PRE_ASSIGNED: String = "isPreAssigned"
+        const val DefaultJobCardNo: String = "JA000"
+        const val JOB_CARD: String = "Job card"
+        const val IS_PRE_ASSIGNED: String = "isPreAssigned"
         const val TOOLBAR_TITLE: String = "toolbar title"
-        const val TRUCK: String = "truck"
         const val ICON_PATH: String = "icon path"
 
         const val SQUARE: String = "square"
-        const val ACCOUNT: String = "account"
 
         const val CAPTURE_PICTURE = 1
 
         const val IMAGE_ROOT_PATH = "images"
 
         const val TEMP_FILE = "temp"//only used when creating a new model
-
-        const val DISCARD_CHANGES = "Discard Changes?"
 
     }
 }
